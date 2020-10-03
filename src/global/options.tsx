a5aa94f9c77899a63516cc8c39ac65627ca72deb
Andrew Oplas <andrewmoplas@gmail.com>
2020-10-03 09:41:44 +0800

    Fix steps prefix

--- src/global/options.tsx
+++ src/global/options.tsx
@@ -20,43 +20,43 @@ export const quantityTypeOptions = [
 export const purchaseRequestActionsOptions = [
 	{
 		value: purchaseRequestActions.NEW,
-		name: '(1/6) New',
+		name: 'New',
 	},
 	{
 		value: purchaseRequestActions.SEEN,
-		name: '(2/6) Seen',
+		name: 'Seen',
 	},
 	{
 		value: purchaseRequestActions.F_OS1_CREATING,
-		name: '(3/6) F-OS1 Creating',
+		name: 'F-OS1 Creating',
 	},
 	{
 		value: purchaseRequestActions.F_OS1_CREATED,
-		name: '(3/6) F-OS1 Created',
+		name: 'F-OS1 Created',
 	},
 	{
 		value: purchaseRequestActions.F_OS1_PREPARING,
-		name: '(4/6) F-OS1 Preparing',
+		name: 'F-OS1 Preparing',
 	},
 	{
 		value: purchaseRequestActions.F_OS1_PREPARED,
-		name: '(4/6) F-OS1 Prepared',
+		name: 'F-OS1 Prepared',
 	},
 	{
 		value: purchaseRequestActions.F_DS1_CREATING,
-		name: '(5/6) F-DS1 Creating',
+		name: 'F-DS1 Creating',
 	},
 	{
 		value: purchaseRequestActions.F_DS1_CREATED,
-		name: '(5/6) F-DS1 Created',
+		name: 'F-DS1 Created',
 	},
 	{
 		value: purchaseRequestActions.F_DS1_DELIVERING,
-		name: '(6/6) F-DS1 Delivering',
+		name: 'F-DS1 Delivering',
 	},
 	{
 		value: purchaseRequestActions.F_DS1_DELIVERED,
-		name: '(6/6) F-DS1 Delivered',
+		name: 'F-DS1 Delivered',
 	},
 ];
 

0ee5bad503bc8ad1108f1a1a1e7794156a8f5a29
Andrew Oplas <andrewmoplas@gmail.com>
2020-10-10 13:44:36 +0800

    Update retry interval

--- src/global/constants.tsx
+++ src/global/constants.tsx
@@ -2,7 +2,7 @@ export const MAX_PAGE_SIZE = 1000000;
 
 export const MAX_RETRY = 3;
 
-export const RETRY_INTERVAL_MS = 300;
+export const RETRY_INTERVAL_MS = 1000;
 
 export const NOT_FOUND_INDEX = -1;
 

356ddcfefbf5853f9044008606d1bc6b2d64c287
Vincent James Maranga <vjmrnga@gmail.com>
2024-08-29 12:33:40 +0800

    Add payment summary file

--- src/screens/Shared/ViewBranchMachine/components/TabPaymentsReceived/components/Summary/index.tsx
+++ src/screens/Shared/ViewBranchMachine/components/TabPaymentsReceived/components/Summary/index.tsx
@@ -1,7 +1,7 @@
 import { Col, Row, Space, Spin, Statistic } from 'antd';
 import { RequestErrors } from 'components';
-import { refetchOptions, timeRangeTypes } from 'global';
-import { useQueryParams, useTransactionsSummary } from 'hooks';
+import { useQueryParams, usePaymentsSummary } from 'hooks';
+import { timeRangeTypes } from 'global';
 import React from 'react';
 import { convertIntoArray, formatInPeso } from 'utils';
 import './style.scss';
@@ -15,46 +15,64 @@ export const Summary = ({ branchMachineId }: Props) => {
 
 	const {
 		data: { summary = null } = {},
-		error: transactionsSummaryError,
-		isLoading: isTransactionsSummaryLoading,
-	} = useTransactionsSummary({
+		isFetching: isFetchingPaymentSummaryData,
+		error: paymentSummaryError,
+	} = usePaymentsSummary({
 		params: {
 			...params,
 			branchMachineId,
 			timeRange: params?.timeRange || timeRangeTypes.DAILY,
-		},
-		options: {
-			...refetchOptions,
-			notifyOnChangeProps: ['data', 'isLoading'],
 		},
 	});
 
 	return (
 		<div className="Summary mb-4">
 			<RequestErrors
-				errors={convertIntoArray(transactionsSummaryError)}
+				errors={convertIntoArray(paymentSummaryError)}
 				withSpaceBottom
 			/>
 
-			<Spin spinning={isTransactionsSummaryLoading}>
+			<Spin spinning={isFetchingPaymentSummaryData}>
 				<Space className="w-100" direction="vertical" size={16}>
 					<Row gutter={[16, 16]}>
 						<Col md={6} sm={8} xs={24}>
-							<Statistic title="Payments Received" value={formatInPeso(0)} />
+							<Statistic
+								title="Payments Received"
+								value={formatInPeso(summary?.total_payments_received)}
+							/>
 						</Col>
 
 						<Col md={6} sm={8} xs={24}>
-							<Statistic title="Cash Sales Invoice" value={formatInPeso(0)} />
-							<Statistic title="Collection Receipt" value={formatInPeso(0)} />
+							<Statistic
+								title="Cash Sales Invoice"
+								value={formatInPeso(summary?.cash_invoice_payments)}
+							/>
+							<Statistic
+								title="Collection Receipt"
+								value={formatInPeso(summary?.collection_receipts_payments)}
+							/>
 						</Col>
 
 						<Col md={6} sm={8} xs={24}>
-							<Statistic title="Cash" value={formatInPeso(0)} />
-							<Statistic title="Check" value={formatInPeso(0)} />
+							<Statistic
+								title="Cash"
+								value={formatInPeso(summary?.cash_payments)}
+							/>
+							<Statistic
+								title="Check"
+								value={formatInPeso(summary?.check_payments)}
+							/>
 						</Col>
 
 						<Col md={6} sm={8} xs={24}>
-							<Statistic title="Other(s)" value={formatInPeso(0)} />
+							<Statistic
+								title="Other(s)"
+								value={formatInPeso(summary?.other_payments)}
+							/>
+							<Statistic
+								title="Cancelled"
+								value={formatInPeso(summary?.cancelled_payments)}
+							/>
 						</Col>
 					</Row>
 				</Space>

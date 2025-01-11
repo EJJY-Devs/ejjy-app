19c786b28a4640d0f112438aeb20d0d0c48fcee2
Vincent James Maranga <vjmrnga@gmail.com>
2025-01-11 12:02:55 +0800

    Fix id error in redeempoints

--- src/components/modals/RedeemPointsModal/index.tsx
+++ src/components/modals/RedeemPointsModal/index.tsx
@@ -4,7 +4,7 @@ import { useUserAuthenticate } from 'ejjy-global';
 import { ErrorMessage, Form, Formik } from 'formik';
 import { useAccountRedeemPoints } from 'hooks';
 import React, { useCallback } from 'react';
-import { convertIntoArray, getLocalApiUrl } from 'utils';
+import { convertIntoArray, getLocalApiUrl, getId } from 'utils';
 import * as Yup from 'yup';
 import { Button, FieldError, FormInputLabel } from '../../elements';
 
@@ -58,7 +58,7 @@ export const RedeemPointsModal = ({ account, onSuccess, onClose }: Props) => {
 
 		await redeemPoints({
 			id: account.id,
-			redeemAuthorizerId: data.id,
+			redeemAuthorizerId: getId(data.id),
 			redeemedPoints: formData.points,
 			redeemRemarks: formData.remarks,
 		});

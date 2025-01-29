96ebd95ec0c72ea370b4f1a687e17dc49618ae3b
Vincent James Maranga <vjmrnga@gmail.com>
2025-01-29 17:33:03 +0800

    Add createRefetchSlice

--- src/screens/Shared/Cart/stores/useBoundStore.tsx
+++ src/screens/Shared/Cart/stores/useBoundStore.tsx
@@ -3,10 +3,12 @@ import { createInterfaceSlice } from './createInterfaceSlice';
 import { createNavigationSlice } from './createNavigationSlice';
 import { createProductsSlice } from './createProductsSlice';
 import { createSearchingSlice } from './createSearchingSlice';
+import { createRefetchSlice } from './createRefetchSlice';
 
 export const useBoundStore = create<any>()((...a) => ({
 	...createInterfaceSlice(...a),
 	...createNavigationSlice(...a),
 	...createProductsSlice(...a),
 	...createSearchingSlice(...a),
+	...createRefetchSlice(...a),
 }));

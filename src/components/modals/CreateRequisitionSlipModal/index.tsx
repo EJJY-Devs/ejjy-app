bdb3e8debc7ea8dea143660fe4093cf67f381b9e
Vincent James Maranga <vjmrnga@gmail.com>
2025-05-28 14:35:38 +0800

    Refactor createRequisitionSlipModal

--- src/components/modals/CreateRequisitionSlipModal/index.tsx
+++ src/components/modals/CreateRequisitionSlipModal/index.tsx
@@ -1,11 +1,17 @@
 import { Button, Col, Modal, Row, Select } from 'antd';
 import { ErrorMessage, Form, Formik } from 'formik';
 import * as Yup from 'yup';
-import { filterOption, getFullName, ServiceType, useUsers } from 'ejjy-global';
+import {
+	filterOption,
+	getFullName,
+	ServiceType,
+	useBranches,
+	useUsers,
+} from 'ejjy-global';
 import React from 'react';
 
 import { MAX_PAGE_SIZE } from 'global';
-import { getLocalApiUrl } from 'utils';
+import { getLocalApiUrl, getLocalBranchId } from 'utils';
 import { FieldError, Label } from '../../elements';
 
 type ModalProps = {
@@ -18,10 +24,12 @@ const formDetails = {
 	defaultValues: {
 		preparedBy: null,
 		approvedBy: null,
+		vendorId: null,
 	},
 	schema: Yup.object().shape({
 		preparedBy: Yup.number().nullable().required().label('Encoder'),
 		approvedBy: Yup.number().nullable().required().label('Requestor'),
+		vendorId: Yup.number().nullable().required().label('Vendor'),
 	}),
 };
 
@@ -32,6 +40,14 @@ export const CreateRequisitionSlipModal = ({
 }: ModalProps) => {
 	// CUSTOM HOOKS
 	const { data: usersData, isFetching: isFetchingUsers } = useUsers({
+		params: { pageSize: MAX_PAGE_SIZE },
+		serviceOptions: {
+			baseURL: getLocalApiUrl(),
+			type: ServiceType.ONLINE,
+		},
+	});
+
+	const { data: branchesData, isFetching: isFetchingBranches } = useBranches({
 		params: { pageSize: MAX_PAGE_SIZE },
 		serviceOptions: {
 			baseURL: getLocalApiUrl(),
@@ -113,6 +129,37 @@ export const CreateRequisitionSlipModal = ({
 								</Select>
 								<ErrorMessage
 									name="approvedBy"
+									render={(error) => <FieldError error={error} />}
+								/>
+							</Col>
+							<Col span={24}>
+								<Label id="vendorId" label="Vendor" spacing />
+								<Select
+									className="w-100"
+									disabled={isFetchingBranches}
+									filterOption={filterOption}
+									optionFilterProp="children"
+									value={values['vendorId']}
+									showSearch
+									onChange={(value) => {
+										setFieldValue('vendorId', value);
+									}}
+								>
+									{branchesData?.list
+										.filter(
+											(branch) => branch?.id !== Number(getLocalBranchId()),
+										)
+										.map((branch) => {
+											const id = branch?.id;
+											return id ? (
+												<Select.Option key={id} value={id}>
+													{branch.name}
+												</Select.Option>
+											) : null;
+										})}
+								</Select>
+								<ErrorMessage
+									name="vendorId"
 									render={(error) => <FieldError error={error} />}
 								/>
 							</Col>

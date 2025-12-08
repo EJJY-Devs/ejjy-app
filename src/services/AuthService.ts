a9707a5d5d6fd112ac1b0a1f6c275dd7dc8ef798
Vincent Maranga <vjmrnga@gmail.com>
2025-12-09 02:35:29 +0800

    Update auth service

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -11,12 +11,27 @@ interface AcquireToken {
 	password: string;
 }
 
+interface Authenticate {
+	login?: string;
+	password?: string;
+	pin?: string;
+	description?: string;
+	branchMachineId?: number;
+	branchId?: number;
+}
+
 const service = {
 	login: async (body: Login, baseURL) =>
 		axios.post('users/login/', body, { baseURL, ...NO_VERIFICATION_CONFIG }),
 
 	retrieve: async (id: number, baseURL) =>
 		axios.get(`users/${id}/`, { baseURL }),
+
+	authenticate: async (body: Authenticate, baseURL) =>
+		axios.post('users/authenticate/', body, {
+			baseURL,
+			...NO_VERIFICATION_CONFIG,
+		}),
 
 	acquireToken: async (body: AcquireToken, baseURL) =>
 		axios.post('tokens/acquire/', body, { baseURL, ...NO_VERIFICATION_CONFIG }),
